a15124effc4cefb45743c608cbc1d313cbcf1e8b
Val Pizzo <valpizzo2@gmail.com>
2022-06-28 15:50:40 -0400

    Added logic for conditional rendering for all pages in App.jsx by creating a top level context to set what page should be viewable.
    This includes some added components for pages that are currently empty, and those file names may be changed with time. They are placeholders for now.
    
    Working functionality right now includes being able to navigate from the "Fresh Talent" section to the artist profile page, and to navigate back to the home page by clicking "Buskin" or the logo in the nav bar.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import Home from './Home.jsx';
+import ArtistProfile from './ArtistProfile.jsx';
 import NavBar from './NavBar.jsx';
+import UserProfile from './UserProfile.jsx';
+import Event from './Event.jsx';
+import ArtistUpdate from './ArtistUpdate.jsx';
 
+export const TopContext = React.createContext();
 
 export default function App() {
   const [logedIn, setLogedIn] = useState(false);
-
+  const [page, setPage] = useState('home');
   return (
     <>
       {/* components */}
-      <div>
-        <NavBar setLogedIn={setLogedIn} />
-      </div>
-      <Home />
+      <TopContext.Provider value={{page, setPage}}>
+        <div>
+          <NavBar setLogedIn={setLogedIn} />
+        </div>
+        {page === 'home' ? <Home /> : <></>}
+        {page === 'artistProfile' ? <ArtistProfile /> : <></>}
+        {page === 'fanDashboard' ? <fanDashboard /> : <></>}
+        {page === 'userProfile' ? <UserProfile /> : <></>}
+        {page === 'event' ? <Event /> : <></>}
+        {page === 'artistDashboard' ? <ArtistProfile /> : <></>}
+        {page === 'artistUpdate' ? <ArtistUpdate /> : <></>}
+      </TopContext.Provider>
     </>
   );
 }
