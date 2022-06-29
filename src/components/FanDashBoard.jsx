7a153cf8c6d2f7f028c109bb190d59b4fa8fab5f
Fangzhuo Xi <fangzhuoxi93@gmail.com>
2022-06-28 20:27:46 -0400

    add style to fandashboard

--- src/components/FanDashBoard.jsx
+++ src/components/FanDashBoard.jsx
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import apiMasters from '../apiMasters.js';
-import { Img, ArtistImg } from './StyledComponents.js'
+import { EventImg, ArtistImg } from './StyledComponents.js';
+import styled from 'styled-components';
 
 export default function FanDashBoard() {
   const [artistsFollowed, setArtistsFollowed] = useState([]);
   const [eventsSaved, setEventsSaved] = useState([]);
 
+  const ArtistMImg = styled(ArtistImg)`
+    height: 100px;
+    width: 100px;
+    margin: 5px;
+  `;
+
+  const EventsMImg = styled(EventImg)`
+  height: 100px;
+  width: 100px;
+  margin: 5px;
+`;
+
   useEffect(() => {
-    // set fan_id = 1;
+    // needs to change fan_id dynamically
     apiMasters.getFanDashBoard(1)
       .then((response) => {
         setArtistsFollowed(response.data.artists);
@@ -24,13 +37,13 @@ export default function FanDashBoard() {
       Total followed artists:
         <p>{artistsFollowed.length}</p>
       {/* list of artist */}
-      {artistsFollowed.map((artist) => (<ArtistImg key={artist.artist_id} alt='artist pic' src={artist.artist_pic} />))}
+      {artistsFollowed.map((artist) => (<ArtistMImg key={artist.artist_id} alt='artist pic' src={artist.artist_pic} />))}
       <br />
       {/* total saved events count */}
       Total saved events:
         <p>{eventsSaved.length}</p>
       {/* list of events */}
-      {eventsSaved.map((event) => (<Img key={event.event_id} alt='artist pic' src={event.event_pic} />))}
+      {eventsSaved.map((event) => (<EventsMImg key={event.event_id} alt='artist pic' src={event.event_pic} />))}
     </>
   );
 }
