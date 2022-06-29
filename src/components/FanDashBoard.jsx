a4cbeea9969f92eb2953b37f42da539bce2f993a
Fangzhuo Xi <fangzhuoxi93@gmail.com>
2022-06-29 11:50:29 -0400

    fix FanDashBoard

--- src/components/FanDashBoard.jsx
+++ src/components/FanDashBoard.jsx
@@ -5,18 +5,20 @@ import styled from 'styled-components';
 import { SavedEvents, FollowedArtists } from './DashBoardTag.jsx';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import FlipCard from './FlipCard.jsx';
+// import FlipCard from './FlipCard.jsx';
 
 const ArtistMImg = styled(ArtistImg)`
-${'' /* height: 100px;
-width: 100px; */}
-margin: 5px;
+  margin: 5px;
 `;
 
 const EventsMImg = styled(EventImg)`
-${'' /* height: 100px;
-width: 100px; */}
-margin: 5px;
+  margin: 5px;
+`;
+
+const Card = styled.div`
+  text-align: center;
+  width: 20%;
+  display: inline-block;
 `;
 
 export default function FanDashBoard({ setPage }) {
@@ -58,23 +60,23 @@ export default function FanDashBoard({ setPage }) {
         </Grid>
         <Grid item xs={7}>
           {artistsFollowed.map((artist) => (
-            <FlipCard
-              key={artist.artist_id}
-              left={<ArtistMImg alt='artist pic' src={artist.artist_pic} onClick={showArtist} id={artist.artist_id} />}
-              right={<h4>{artist.artist_display_name}</h4>}
-            />))}
+            <Card key={artist.artist_pic}>
+              <ArtistMImg alt='artist pic' src={artist.artist_pic} onClick={showArtist} id={artist.artist_id} />
+              <h5>{artist.artist_display_name}</h5>
+            </Card>
+          ))}
         </Grid>
         <Grid item xs={5}>
           {SavedEvents(eventsSaved.length)}
         </Grid>
         <Grid item xs={7}>
-          {/* {eventsSaved.map((event) => (
-            <Card key={event.event_id}>
+          {eventsSaved.map((event) => (
+            <Card key={event.event_pic}>
               <EventsMImg alt='event pic' src={event.event_pic} onClick={showEvent} id={event.event_id} />
               <h5>{event.event_name}</h5>
-              <h5>{event.event_date}</h5>
+              <h5>{`${event.event_date} ${event.event_start_time}`}</h5>
             </Card>
-          ))} */}
+          ))}
         </Grid>
       </Grid>
     </Box>
