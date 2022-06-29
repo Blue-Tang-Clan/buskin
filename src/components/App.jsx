4b7cac6075dec65578511d5283a50c01434accb1
Yao Yu <amyyuyao.thu@gmail.com>
2022-06-29 16:55:02 -0400

    fix Nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Home from './Home.jsx';
 import ArtistProfile from './ArtistProfile.jsx';
 import NavBar from './NavBar.jsx';
-import UserProfile from './UserProfile.jsx';
+import FanProfile from './FanProfile.jsx';
 import Event from './Event.jsx';
 import ArtistUpdate from './ArtistUpdate.jsx';
 import FanDashBoard from './FanDashBoard.jsx';
@@ -22,12 +22,12 @@ export default function App() {
       {/* components */}
       <TopContext.Provider value={{page, setPage, pageId, setPageId}}>
         <div>
-          <NavBar userType={userType} setUserType={setUserType} />
+          <NavBar userType={userType} setUserId={setUserId} setUserType={setUserType} />
         </div>
         {page === 'home' ? <Home /> : <></>}
         {page === 'artistProfile' ? <ArtistProfile /> : <></>}
         {page === 'fanDashboard' ? <FanDashBoard setPage={setPage} setPageId={setPageId} /> : <></>}
-        {page === 'userProfile' ? <UserProfile /> : <></>}
+        {page === 'fanProfile' ? <FanProfile /> : <></>}
         {page === 'event' ? <Event /> : <></>}
         {page === 'artistDashboard' ? <ArtistProfile /> : <></>}
         {page === 'artistUpdate' ? <ArtistUpdate /> : <></>}
