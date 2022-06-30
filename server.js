204e3bf115ce62a408ac5859626d9ac3f32033ea
nelliott82 <nikkoelliott@gmail.com>
2022-06-30 13:13:26 -0400

    Got rid of router use and added .get for artist/:artistId

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const { router } = require('./router.js');
 
 app.use(express.static('public'));
 
-app.use('/', router);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.get('/artist/:artistId', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
